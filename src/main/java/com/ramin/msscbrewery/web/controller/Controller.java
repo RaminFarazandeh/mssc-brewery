2604cb53d23cbc60cbdff9c7728e5039d5c7d832
Ramin Farazandeh <ramin.farazandeh@cas.de>
2022-06-21 17:07:20 +0200

    updated Controller

--- src/main/java/com/ramin/msscbrewery/web/controller/Controller.java
+++ src/main/java/com/ramin/msscbrewery/web/controller/Controller.java
@@ -1,6 +1,27 @@
-package com.ramin.msscbrewery.web.controller;/**
+package com.ramin.msscbrewery.web.controller;
+
+import com.ramin.msscbrewery.web.model.BeerDto;
+import java.util.UUID;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
  * [description]
  *
  * @author Ramin.Farazandeh
- */public class Controller {
+ */
+@RequestMapping("/api/v1/beer")
+@RestController
+public class Controller {
+
+	@GetMapping("/beerId")
+	public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId") UUID beerId) {
+
+		return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+	}
+
 }
